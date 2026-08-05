d92ef05078edc6f8f142f266398dcdb44fde69ed
YYashSingh-9 <yyashsngh@gmail.com>
2026-08-05 04:35:57 -0700

    about page updated

--- src/Components/Top-level-components/AboutPage.jsx
+++ src/Components/Top-level-components/AboutPage.jsx
@@ -10,26 +10,44 @@ const AboutPage = () => {
         <Box className={classes.parentLayer}>
           <Box className={classes.firstLayer}>
             <Box>
-              <p>This is about me</p>
-              <button>Read more</button>
+              <p>
+                Thinking why should you{" "}
+                <span className={classes.highlighted_text}> hire me</span>{" "}
+                ?{" "}
+              </p>
+              <button>Read this please..</button>
             </Box>
             <Box>
-              <p>This is also about me</p> <button>Read more</button>
+              <p>
+                Why i{" "}
+                <span className={classes.highlighted_text}> stand out</span>{" "}
+                from the crowd.
+              </p>{" "}
+              <button>Click here please.</button>
             </Box>
           </Box>
           <Box className={classes.secondLayer}>
-            <p>About me</p>
+            <p>About me.</p>
             <Box>
               <img src={vaseImg} />
             </Box>
           </Box>
           <Box className={classes.thirdLayer}>
             <Box>
-              <p>This is about me</p>
-              <button>Read more</button>
+              <p>
+                How i am able to{" "}
+                <span className={classes.highlighted_text}>
+                  {" "}
+                  contribute more.
+                </span>
+              </p>
+              <button>It would be great, if you read this.</button>
             </Box>
             <Box>
-              <p>This is also about me</p> <button>Read more</button>
+              <p>My blunt honest opinion.</p>
+              <button>
+                Very direct but explains my true self, please read.
+              </button>
             </Box>
           </Box>
         </Box>
